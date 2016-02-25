835538f8159f95cebd33219391b0fbed8f6c2c1d
jianfeng.zhang <jianfeng.zhang@viziner.cn>
2016-02-25 16:42:07 +0800

    Initial Commit

---
+++ DMShortVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DMShortVideo
+//
+//  Created by viziner on 16/2/25.
+//  Copyright © 2016年 idemonolater. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
